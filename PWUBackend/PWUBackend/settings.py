4faf258eceea2ba97ddb3acf071b014db2ea2e96
himangshuishere <dehimangshu2020@outlook.com>
2022-03-08 16:33:30 +0530

    Cookies on Hold
    Models Refactored.

--- PWUBackend/PWUBackend/settings.py
+++ PWUBackend/PWUBackend/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 REST_FRAMEWORK={
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
