c81aa30eec3321a19807fc7f47b9de697c6f81b8
himangshuishere <dehimangshu2020@outlook.com>
2022-03-11 11:21:13 +0530

    Fixatures done.
    Created custom user model.
    Resolved the problem of UUID as primary key
    This project has postgresql as its backend

--- PWUBackend/PWUBackend/settings.py
+++ PWUBackend/PWUBackend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,8 +82,12 @@ WSGI_APPLICATION = 'PWUBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'printwithus',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -126,6 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- PWUBackend/accounts/serializers.py
+++ PWUBackend/accounts/serializers.py
@@ -4,61 +4,57 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
 
-from .models import UserData
+from .models import User
 
 
-class UserDataRegistrationSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     class Meta:
-        model = UserData
-        fields = ['username', 'password', 'password2', 'email', 'user_phone']
+        model = User
+        fields = [ 'email', 'password', 'password2']
         extra_kwargs = {'password': {'write_only': True}}
 
     def save(self):
         if self.validated_data['password'] != self.validated_data['password2']:
             raise ValidationError({'error': 'Passwords must match'})
         
-        if ('email' not in self.validated_data and 'user_phone' not in self.validated_data) or (self.validated_data['email'] == "" and self.validated_data['user_phone'] == ""):
-            raise ValidationError({'error': 'Email or Phone is required'})
+        if ('email' not in self.validated_data ):
+            raise ValidationError({'error': 'Email is required'})
         
-        user = UserData.objects.create(
-            username=self.validated_data['username'],
+        user = User.objects.create(
             email=self.validated_data['email'],
-            user_phone=self.validated_data['user_phone'],
-            password=self.validated_data['password2']
         )
+        user.set_password(self.validated_data['password2'])
         user.save()
         return user
 
-class UserDataLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserData
-        fields = ['email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'})
     
-    def save(self):
+    def save(self, *args, **kwargs):
         try:
-            a = UserData.objects.get(email=self.validated_data['email'])
+            a = User.objects.filter(email=self.validated_data['email']).exists()
             if a:
-                passData = a.password
-                if passData == self.validated_data['password']:
-                    token, created= Token.objects.get_or_create(user=a)
-                    return {'token': token.key, 'user_id': a.user_id}
+                passData = User.objects.get(email=self.validated_data['email']).check_password(self.validated_data['password'])
+                if passData:
+                    token, created = Token.objects.get_or_create(user=User.objects.get(email=self.validated_data['email']))
+                    return {'token': token.key, 'user_id': User.objects.get(email=self.validated_data['email']).id}
                 raise ValidationError({'error': 'Password Invalid'}) 
         except User.DoesNotExist:
             raise ValidationError({"error":"Account or Email Does Not Exist"})
         
 
 
-class UserDataMerchantLoginSerializer(serializers.ModelSerializer):
+class UserMerchantLoginSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserData
-        fields = ['user_phone', 'password']
+        model = User
+        fields = ['mobile', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def save(self):
         try:
-            a = UserData.objects.get(user_phone=self.validated_data['user_phone'])
+            a = User.objects.get(mobile=self.validated_data['mobile'])
             if a:
                 passData = a.password
                 if passData == self.validated_data['password']:
@@ -66,9 +62,9 @@ class UserDataMerchantLoginSerializer(serializers.ModelSerializer):
                     return {'token': token.key, 'user_id': a.user_id}
                 raise ValidationError({'error': 'Password Invalid'}) 
         except User.DoesNotExist:
-            raise ValidationError({"error":"Merchant Account or Phone Does Not Exist"})
+            raise ValidationError({"error":"Merchant Account or mobile Does Not Exist"})
 
-class UserDataSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserData
-        fields = ['first_name','last_name', 'username','email', 'user_address', 'user_phone', 'user_profile_pic', 'user_id']
+        model = User
+        fields = ['id', 'first_name','last_name', 'email', 'user_address', 'mobile', 'user_profile_pic', 'is_merchant']
