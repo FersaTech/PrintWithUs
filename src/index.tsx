2f5a388e9a28b100da08d37523fdc31f23f0bec5
Dust Of Snow <new.to.world2003@gmail.com>
2022-04-11 08:12:28 +0530

    Tailwind Setup Generated -- Starting the Project

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const container = document.getElementById('root');
+if(!container) throw new Error("Failed to find the root Element");
+const root = createRoot(container);
+root.render(<App />);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
