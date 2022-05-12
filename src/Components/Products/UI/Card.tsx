be7a1afad0d1f19442d8ee89bfc4ad40079af0e6
ThEArUnOdAyA <coder.india.01@gmail.com>
2022-05-12 18:15:53 +0530

    Fixed UI Proposal -- #13     Fixed UI data layout for Card Component. Now it is more Generalised and can be used in any Component as Generalised using props

--- src/Components/Products/UI/Card.tsx
+++ src/Components/Products/UI/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Card = (props: any) => {
   return (
-    <div className="card gap-8 md:flex md:flex-wrap 2xl:grid 2xl:grid-cols-3 md:justify-items-center mt-28">
+    <div className={`card gap-8 md:flex md:flex-wrap 2xl:grid 2xl:grid-cols-3 md:justify-items-center mt-28 ${props.className}`}>
       <div className="py-10 mx-auto ">
         <div className="rounded-lg overflow-hidden shadow-lg max-w-sm">
           {props.children}
